43d9b7c85a61fcb1893798ddc32a505804d26602
Psilon <pzixel@gmail.com>
2018-12-09 19:53:48 +0300

    second

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use failure::{bail, Error, err_msg};
 use goblin::pe::PE;
+use goblin::pe::data_directories::DataDirectory;
+use goblin::pe::section_table::SectionTable;
 
 fn main() -> Result<(), Error> {
     let path = std::env::args()
@@ -13,7 +15,17 @@ fn main() -> Result<(), Error> {
     }
     let optional_header = pe.header.optional_header.ok_or_else(|| err_msg("No optional header"))?;
     let cli_header = optional_header.data_directories.data_directories[14].ok_or_else(|| err_msg("No CLI header"))?;
+    let section = get_section(&pe, &cli_header)?;
     println!("{:#?}", pe);
     println!("{:?}", cli_header.virtual_address);
     Ok(())
+}
+
+fn get_section<'a>(pe: &'a PE, header: &DataDirectory) -> Result<&'a SectionTable, Error> {
+    for section in pe.sections.iter() {
+        if header.virtual_address >= section.virtual_address && header.virtual_address < section.virtual_address + header.size {
+            return Ok(&section);
+        }
+    }
+    bail!("Section for address {} was not found", header.virtual_address)
 }
