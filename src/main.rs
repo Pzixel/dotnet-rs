30a1f52a4109d0ebf25c7af4872932c5731be24f
Psilon <pzixel@gmail.com>
2018-12-09 18:39:14 +0300

    Determine if file is .Net executable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use failure::{bail, Error};
+use goblin::pe::PE;
+
+fn main() -> Result<(), Error> {
+    let path = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Path to executable was not specified");
+    let file = std::fs::read(path)?;
+    let pe = PE::parse(&file)?;
+    if pe.header.coff_header.machine != 0x14c {
+        bail!("Is not a .Net executable");
+    }
+    println!("Hello, world! {:?}", pe.header);
+    Ok(())
 }
